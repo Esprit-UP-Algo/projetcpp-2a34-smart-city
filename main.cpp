0813502c7d7d8da5d0ff92ff265b531f51e81bd5
OmarBenothmen <omarbenothmen71@gmail.com>
2025-09-29 11:14:41 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
+// main.cpp
+#include <QApplication>
 #include "mainwindow.h"
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();
+    return app.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,41 @@
+// mainwindow.h
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QGroupBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+
+private slots:
+    void addLocal();
 
 private:
-    Ui::MainWindow *ui;
+    QWidget *createLocauxInterface();
+
+    QLineEdit *nomEdit;
+    QLineEdit *numEdit;
+    QSpinBox  *aireSpin;
+    QLineEdit *locEdit;
+    QSpinBox  *chambresSpin;
+    QComboBox *typeCombo;
+    QComboBox *statutCombo;
+    QSpinBox  *prixSpin;
+
+    QTableWidget *table;
 };
+
 #endif // MAINWINDOW_H
