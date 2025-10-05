5905507afaa65ad9e86e13166e5f3ec46a52a18d
nour <hmaoui.nour@gmail.com>
2025-10-06 01:05:53 +0200

    la nouvelle versin de gestion parking

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,255 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QGroupBox *groupBox;
+    QLineEdit *lineEdit;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLineEdit *lineEdit_2;
+    QLineEdit *lineEdit_3;
+    QLineEdit *lineEdit_4;
+    QLineEdit *lineEdit_5;
+    QLineEdit *lineEdit_8;
+    QCheckBox *checkBox;
+    QCheckBox *checkBox_2;
+    QCheckBox *checkBox_3;
+    QCheckBox *checkBox_4;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton;
+    QTableWidget *tableWidget;
+    QLineEdit *lineEdit_6;
+    QPushButton *pushButton_4;
+    QLabel *label_9;
+    QLabel *label_10;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(846, 600);
+        MainWindow->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName("groupBox");
+        groupBox->setGeometry(QRect(90, 80, 311, 481));
+        groupBox->setStyleSheet(QString::fromUtf8("background-color: rgb(191, 222, 193);"));
+        lineEdit = new QLineEdit(groupBox);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(130, 60, 113, 26));
+        label = new QLabel(groupBox);
+        label->setObjectName("label");
+        label->setGeometry(QRect(10, 60, 91, 20));
+        label_2 = new QLabel(groupBox);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(10, 110, 63, 20));
+        label_3 = new QLabel(groupBox);
+        label_3->setObjectName("label_3");
+        label_3->setGeometry(QRect(10, 150, 81, 20));
+        label_4 = new QLabel(groupBox);
+        label_4->setObjectName("label_4");
+        label_4->setGeometry(QRect(10, 190, 63, 20));
+        label_5 = new QLabel(groupBox);
+        label_5->setObjectName("label_5");
+        label_5->setGeometry(QRect(10, 240, 101, 20));
+        label_6 = new QLabel(groupBox);
+        label_6->setObjectName("label_6");
+        label_6->setGeometry(QRect(10, 280, 63, 20));
+        label_7 = new QLabel(groupBox);
+        label_7->setObjectName("label_7");
+        label_7->setGeometry(QRect(10, 320, 63, 20));
+        label_8 = new QLabel(groupBox);
+        label_8->setObjectName("label_8");
+        label_8->setGeometry(QRect(10, 360, 91, 20));
+        lineEdit_2 = new QLineEdit(groupBox);
+        lineEdit_2->setObjectName("lineEdit_2");
+        lineEdit_2->setGeometry(QRect(130, 110, 113, 26));
+        lineEdit_3 = new QLineEdit(groupBox);
+        lineEdit_3->setObjectName("lineEdit_3");
+        lineEdit_3->setGeometry(QRect(130, 150, 113, 26));
+        lineEdit_4 = new QLineEdit(groupBox);
+        lineEdit_4->setObjectName("lineEdit_4");
+        lineEdit_4->setGeometry(QRect(130, 190, 113, 26));
+        lineEdit_5 = new QLineEdit(groupBox);
+        lineEdit_5->setObjectName("lineEdit_5");
+        lineEdit_5->setGeometry(QRect(130, 230, 113, 26));
+        lineEdit_8 = new QLineEdit(groupBox);
+        lineEdit_8->setObjectName("lineEdit_8");
+        lineEdit_8->setGeometry(QRect(130, 360, 113, 26));
+        checkBox = new QCheckBox(groupBox);
+        checkBox->setObjectName("checkBox");
+        checkBox->setGeometry(QRect(100, 280, 91, 24));
+        checkBox_2 = new QCheckBox(groupBox);
+        checkBox_2->setObjectName("checkBox_2");
+        checkBox_2->setGeometry(QRect(190, 280, 91, 24));
+        checkBox_3 = new QCheckBox(groupBox);
+        checkBox_3->setObjectName("checkBox_3");
+        checkBox_3->setGeometry(QRect(100, 310, 91, 24));
+        checkBox_4 = new QCheckBox(groupBox);
+        checkBox_4->setObjectName("checkBox_4");
+        checkBox_4->setGeometry(QRect(190, 310, 91, 24));
+        pushButton_3 = new QPushButton(groupBox);
+        pushButton_3->setObjectName("pushButton_3");
+        pushButton_3->setGeometry(QRect(210, 410, 93, 29));
+        pushButton_3->setStyleSheet(QString::fromUtf8("background-color: rgb(76, 122, 69);\n"
+"    color: white;               /* texte blanc */\n"
+"    border-radius: 10px;        /* coins arrondis */\n"
+"    padding: 5px"));
+        pushButton_2 = new QPushButton(groupBox);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(110, 410, 93, 29));
+        pushButton_2->setStyleSheet(QString::fromUtf8("background-color: rgb(76, 122, 69);\n"
+"    color: white;               /* texte blanc */\n"
+"    border-radius: 10px;        /* coins arrondis */\n"
+"    padding: 5px"));
+        pushButton = new QPushButton(groupBox);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(10, 410, 93, 29));
+        pushButton->setStyleSheet(QString::fromUtf8("background-color: rgb(76, 122, 69);\n"
+"    color: white;               /* texte blanc */\n"
+"    border-radius: 10px;        /* coins arrondis */\n"
+"    padding: 5px"));
+        tableWidget = new QTableWidget(centralwidget);
+        if (tableWidget->columnCount() < 7)
+            tableWidget->setColumnCount(7);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(1, __qtablewidgetitem1);
+        QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(2, __qtablewidgetitem2);
+        QTableWidgetItem *__qtablewidgetitem3 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(3, __qtablewidgetitem3);
+        QTableWidgetItem *__qtablewidgetitem4 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(4, __qtablewidgetitem4);
+        QTableWidgetItem *__qtablewidgetitem5 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(5, __qtablewidgetitem5);
+        QTableWidgetItem *__qtablewidgetitem6 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(6, __qtablewidgetitem6);
+        tableWidget->setObjectName("tableWidget");
+        tableWidget->setGeometry(QRect(420, 90, 401, 421));
+        tableWidget->setStyleSheet(QString::fromUtf8("background-color: rgb(191, 222, 193);"));
+        lineEdit_6 = new QLineEdit(centralwidget);
+        lineEdit_6->setObjectName("lineEdit_6");
+        lineEdit_6->setGeometry(QRect(90, 45, 311, 31));
+        lineEdit_6->setStyleSheet(QString::fromUtf8("alternate-background-color: rgb(255, 255, 127);\n"
+"background-color: rgb(222, 255, 215);"));
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName("pushButton_4");
+        pushButton_4->setGeometry(QRect(420, 0, 131, 29));
+        pushButton_4->setStyleSheet(QString::fromUtf8("background-color: rgb(76, 122, 69);\n"
+"    color: white;               /* texte blanc */\n"
+"    border-radius: 10px;        /* coins arrondis */\n"
+"    padding: 5px"));
+        label_9 = new QLabel(centralwidget);
+        label_9->setObjectName("label_9");
+        label_9->setGeometry(QRect(410, 50, 251, 31));
+        label_9->setStyleSheet(QString::fromUtf8("  /* bleu */\n"
+"background-color: rgb(195, 238, 176);\n"
+"    color: black;               /* texte blanc */\n"
+"    border-radius: 10px;        /* coins arrondis */\n"
+"    padding: 5px"));
+        label_10 = new QLabel(centralwidget);
+        label_10->setObjectName("label_10");
+        label_10->setGeometry(QRect(690, -30, 121, 111));
+        label_10->setPixmap(QPixmap(QString::fromUtf8("../../Downloads/2e5aa8cc-e6fc-460b-b6b1-0e09a951bfdc.jpg")));
+        label_10->setScaledContents(true);
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 846, 26));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "Ajouter/modifier un parking ", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "ID parking", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Nom ", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "localisation", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "Capacit\303\251", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "Places libres", nullptr));
+        label_6->setText(QCoreApplication::translate("MainWindow", "Statut", nullptr));
+        label_7->setText(QCoreApplication::translate("MainWindow", "Type", nullptr));
+        label_8->setText(QCoreApplication::translate("MainWindow", "Tarification", nullptr));
+        checkBox->setText(QCoreApplication::translate("MainWindow", "ouvert", nullptr));
+        checkBox_2->setText(QCoreApplication::translate("MainWindow", "ferm\303\251", nullptr));
+        checkBox_3->setText(QCoreApplication::translate("MainWindow", "Public", nullptr));
+        checkBox_4->setText(QCoreApplication::translate("MainWindow", "Priv\303\251", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Supprimer", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Modifier", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Ajouter", nullptr));
+        QTableWidgetItem *___qtablewidgetitem = tableWidget->horizontalHeaderItem(0);
+        ___qtablewidgetitem->setText(QCoreApplication::translate("MainWindow", "Nom", nullptr));
+        QTableWidgetItem *___qtablewidgetitem1 = tableWidget->horizontalHeaderItem(1);
+        ___qtablewidgetitem1->setText(QCoreApplication::translate("MainWindow", "Localisation", nullptr));
+        QTableWidgetItem *___qtablewidgetitem2 = tableWidget->horizontalHeaderItem(2);
+        ___qtablewidgetitem2->setText(QCoreApplication::translate("MainWindow", "Capacit\303\251", nullptr));
+        QTableWidgetItem *___qtablewidgetitem3 = tableWidget->horizontalHeaderItem(3);
+        ___qtablewidgetitem3->setText(QCoreApplication::translate("MainWindow", "Places libres", nullptr));
+        QTableWidgetItem *___qtablewidgetitem4 = tableWidget->horizontalHeaderItem(4);
+        ___qtablewidgetitem4->setText(QCoreApplication::translate("MainWindow", "Statut", nullptr));
+        QTableWidgetItem *___qtablewidgetitem5 = tableWidget->horizontalHeaderItem(5);
+        ___qtablewidgetitem5->setText(QCoreApplication::translate("MainWindow", "Type", nullptr));
+        QTableWidgetItem *___qtablewidgetitem6 = tableWidget->horizontalHeaderItem(6);
+        ___qtablewidgetitem6->setText(QCoreApplication::translate("MainWindow", "Tarif", nullptr));
+        lineEdit_6->setText(QCoreApplication::translate("MainWindow", "Rechercher un parking", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "Rafraichir", nullptr));
+        label_9->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p>Liste des parkings</p></body></html>", nullptr));
+        label_10->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,120 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QSplitter>
+#include <QGroupBox>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QDateTimeEdit>
+#include <QPushButton>
+#include <QTableWidget>
+#include <QHeaderView>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent) {
+    QWidget *central = new QWidget;
+    QHBoxLayout *rootLayout = new QHBoxLayout(central);
+
+    QWidget *sidebar = new QWidget;
+    sidebar->setFixedWidth(200);
+    QVBoxLayout *sidebarLayout = new QVBoxLayout(sidebar);
+    sidebarLayout->setContentsMargins(6,20,6,20);
+    sidebarLayout->setSpacing(12);
+
+    QStringList buttons = {
+        "Gestion des locaux",
+        "Gestion des parkings intelligents",
+        "Gestion des equipements",
+        "Gestion employe",
+        "Gestion des dechts",
+        "Gestion d'alerte"
+    };
+
+    for(auto &text : buttons){
+        QPushButton *b = new QPushButton(text);
+        b->setProperty("sky", true);
+        b->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+        sidebarLayout->addWidget(b);
+    }
+
+    QWidget *parkingPage = createParkingPage();
+
+    rootLayout->addWidget(sidebar);
+    rootLayout->addWidget(parkingPage);
+
+    setCentralWidget(central);
+    setWindowTitle("Gestion de parking");
+    resize(1200,700);
+
+    central->setStyleSheet(R"(
+        QWidget{background-color:#e6f9e6;}
+        QPushButton{background-color:#0b6623;color:white;padding:6px;border-radius:6px;min-height:36px;}
+        QPushButton[sky="true"]{background-color:#87ceeb;color:black;font-weight:bold;}
+        QLineEdit,QComboBox,QDateTimeEdit{background:white;padding:4px;border:1px solid #ccc;border-radius:4px;}
+        QTableWidget{background:white;gridline-color:#ccc;}
+        QHeaderView::section{background-color:#0b6623;color:white;padding:4px;}
+    )");
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+QWidget* MainWindow::createParkingPage(){
+    QWidget *page = new QWidget;
+    QVBoxLayout *mainLayout = new QVBoxLayout(page);
+
+    QHBoxLayout *topBar = new QHBoxLayout;
+    QLineEdit *search = new QLineEdit;
+    search->setPlaceholderText("Rechercher...");
+    QPushButton *refreshBtn = new QPushButton("Rafraîchir");
+    QPushButton *exportBtn = new QPushButton("Exporter");
+    topBar->addWidget(search);
+    topBar->addWidget(refreshBtn);
+    topBar->addWidget(exportBtn);
+    mainLayout->addLayout(topBar);
+
+    QSplitter *split = new QSplitter;
+
+    QWidget *formWidget = new QWidget;
+    QVBoxLayout *formLayout = new QVBoxLayout(formWidget);
+    QGroupBox *formGroup = new QGroupBox("Ajouter / Modifier Parking");
+    QGridLayout *grid = new QGridLayout(formGroup);
+
+    QLineEdit *idEdit = new QLineEdit;
+    QComboBox *typeCombo = new QComboBox; typeCombo->addItems({"Public","Privé","Souterrain"});
+    QLineEdit *NomParkingEdit = new QLineEdit;
+    QLineEdit *PlaceslibresEdit = new QLineEdit;
+    QComboBox *LocalisationCombo = new QComboBox; LocalisationCombo->addItems({"GPS","Adresse"});
+    QLineEdit *CapacitéTotaleEdit = new QLineEdit;
+    QComboBox *statutCombo = new QComboBox; statutCombo->addItems({"Ouverte","Fermé"});
+    QLineEdit *TarifHoraireEdit = new QLineEdit;
+
+    grid->addWidget(new QLabel("ID:"),0,0); grid->addWidget(idEdit,0,1);
+    grid->addWidget(new QLabel("Type:"),1,0); grid->addWidget(typeCombo,1,1);
+    grid->addWidget(new QLabel("NomParking:"),2,0); grid->addWidget(NomParkingEdit,2,1);
+    grid->addWidget(new QLabel("Placeslibres:"),3,0); grid->addWidget(PlaceslibresEdit,3,1);
+    grid->addWidget(new QLabel("Localisation:"),4,0); grid->addWidget(LocalisationCombo,4,1);
+    grid->addWidget(new QLabel("CapacitéTotale:"),5,0); grid->addWidget(CapacitéTotaleEdit,5,1);
+    grid->addWidget(new QLabel("Statut:"),6,0); grid->addWidget(statutCombo,6,1);
+    grid->addWidget(new QLabel("TarifHoraire:"),7,0); grid->addWidget(TarifHoraireEdit,7,1);
+    QHBoxLayout *actions = new QHBoxLayout;
+    actions->addWidget(new QPushButton("Ajouter"));
+    actions->addWidget(new QPushButton("Modifier"));
+    actions->addWidget(new QPushButton("Supprimer"));
+
+    formLayout->addWidget(formGroup);
+    formLayout->addLayout(actions);
+
+    QTableWidget *table = new QTableWidget;
+    table->setColumnCount(7);
+    table->setHorizontalHeaderLabels({"ID","Type","NomParking","Placeslibres","Localisation","CapacitéTotale","Statut","TarifHoraire"});
+    table->horizontalHeader()->setStretchLastSection(true);
+    table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    table->setSelectionBehavior(QAbstractItemView::SelectRows);
+    table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    split->addWidget(formWidget);
+    split->addWidget(table);
+    split->setStretchFactor(1,1);
+
+    mainLayout->addWidget(split);
+    return page;
 }
